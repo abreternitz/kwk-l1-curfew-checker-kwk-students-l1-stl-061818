c214b0ffc159668f89edcaa734f233aaf7fcc746
abreternitz <aliceburian@gmail.com>
2018-06-19 19:03:27 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -11,7 +11,19 @@ def complex_curfew_checker(time)
 end
 
 def deluxe_curfew_checker(time)
-  # code goes here
+  def curfew_checker(arrived)
+  arrived =time-11
+  if arrived>11
+    puts "you're #{time}hour(s) late"
+  elsif arrived==11
+    puts"you're on time"
+  else 
+    puts "you're #{time} hour(s) early"
+  end
+end
+
+curfew_checker(12)
+
 end
 
 def platinum_curfew_checker(current_time, curfew_time)
