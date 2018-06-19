e59c89391b7cc7b28ac59db2c6a7766a4a408b66
abreternitz <aliceburian@gmail.com>
2018-06-19 18:51:11 +0000

    automatically backed up by learn

--- newfile.rb
+++ newfile.rb
@@ -1,11 +1,11 @@
 def curfew_checker(curfew)
-  if curfew=="early"
+  if curfew<=11
     puts "you're early"
-  elsif curfew == "late"
+  elsif curfew >=11
     "you're late"
   else 
     puts "you're ontime"
   end
 end
 
-curfew_checker("ontime")
+curfew_checker(11)
