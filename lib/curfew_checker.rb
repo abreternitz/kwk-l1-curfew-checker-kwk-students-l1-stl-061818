689611082a157adacb290b1f6ff8f365465e12b2
abreternitz <aliceburian@gmail.com>
2018-06-19 19:08:36 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -16,6 +16,13 @@ end
 
 def complex_curfew_checker(time)
   # code goes here
+  if time==11
+    puts "Time to apparate!"
+  elsif time>11
+   puts "You're in trouble! Better get home quick!"
+  else
+    puts "Keep having fun!"
+  end
 end
 
 def deluxe_curfew_checker(time)
