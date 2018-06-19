98f637164df5ef4a037c61299a7a3eeadeb8b5cc
abreternitz <aliceburian@gmail.com>
2018-06-19 19:00:14 +0000

    automatically backed up by learn

--- newfile.rb
+++ newfile.rb
@@ -1,10 +1,11 @@
 def curfew_checker(arrived)
+  arrived - 11=time
   if arrived>11
-    puts "you're hour(s) late"
+    puts "you're #{time}hour(s) late"
   elsif arrived==11
     puts"you're on time"
   else 
-    puts "you're hour(s) early"
+    puts "you're #{time} hour(s) early"
   end
 end
 
